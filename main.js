a945a0f27e102e693bb8f25017e6340d54eaf356
larissa <analauracarvalho3991@gmail.com>
2025-05-04 19:56:47 -0300

    COMECO E FIM DOS EXERCICIOS

---
+++ main.js
@@ -0,0 +1,79 @@
+/*
+# Exercício 1 
+
+function mensagem() {
+    console.log("seja bem-vindo(a)!")
+    console.log("É um prazer tê-lo aqui.")
+    console.log("Aproveite")
+}
+
+mensagem()
+
+*/
+
+/*
+# exercício 2 (COM PARAMETROS)
+
+function somarNumeros((COM PARAMETROS)) {
+    let soma = valor1 + valor2 
+    console.log(soma)
+}
+
+console.log(soma)
+somarNumeros(15, 27)
+
+*/
+
+/*
+# exercício 3 (COM PARAMETROS)
+
+function boasVindas(nome, cargo) {
+    console.log(`Seja bem-vindo(a) ${cargo} ${nome}!`)
+}
+
+boasVindas("Marcio", "professor")
+
+*/
+
+/*
+# exercício 4 (COM RETURN)
+
+function somar(valor1, valor2) {
+    return valor1 + valor2
+}
+
+let resultado = somar(29, 32)
+console.log(somar(12, 12))
+
+console.log(resultado)
+
+*/
+
+/*
+# exercício 4 (COM RETURN)
+
+function boasVindas(nome, cargo) {
+    return `Seja bem-vindo(a) ${cargo} ${nome}!`
+}
+let mensagem = boasVindas("Marcio", "professor")
+
+console.log(mensagem)
+
+*/
+
+/*
+# exercício 5 (CONDICIONAL COM ELSE)
+
+function parImpar(valor){
+    if (valor % 2 === 0) {
+        return "par"
+    } else {
+
+    }
+}
+
+let verificao = parImpar(13)
+
+console.log(verificao)
+
+*/
